27747005736dece7b5fcad9401f3aea152189213
GregorySmok <g.smokovdin@yandex.ru>
2024-06-05 22:29:29 +0300

    dff

--- main.py
+++ main.py
@@ -5,3 +5,22 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+def ficha():
+    print('КУ КУ')
+    print('ban')
+
+
+def fich():
+    print('alfa')
+
+
+def main():
+    k = 5
+    print(k)
+    ficha()
+
+
+if __name__ == '__main__':
+    main()
